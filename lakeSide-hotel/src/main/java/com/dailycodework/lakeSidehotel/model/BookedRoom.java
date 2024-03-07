3422759e4bd92e401079e723d992a9224fcb75fa
astrataraxia <astrataraxia@gmail.com>
2024-03-07 12:29:29 +0900

    hotel-backend

---
+++ lakeSide-hotel/src/main/java/com/dailycodework/lakeSidehotel/model/BookedRoom.java
@@ -0,0 +1,70 @@
+package com.dailycodework.lakeSidehotel.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numOfAdults;
+
+    @Column(name = "children")
+    private int numOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumOfGuests;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        totalNumOfGuests = numOfAdults + numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+}
